cf6c3e85f9546a7ce4a8bb9f3d42263bce587f74
Ruxandra <filotiruxandra@gmail.com>
2025-05-27 18:47:08 +0300

    displayed the data for every station with different url

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template #Flask = class
+import pandas as pd #import pandas library
 
 app = Flask(__name__)
+
+
 
 #integrate the index.html in main.py
 @app.route("/")
@@ -9,7 +12,9 @@ def home():
 
 @app.route("/api/v1/<station>/<date>") #create a route for the API and is dynamic (station and date)
 def about(station, date):
-    temperature = 23
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt" #takes the path of file and reads it
+    df= pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"]) #skips some rows in the file
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10 #locates the date and squeezes the value to get the temperature
     return {"station": station,
             "date": date,
              "temperature": temperature}
