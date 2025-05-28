e79912e6bc1894f23bb64cf608050b87abe7cd4f
Ruxandra <filotiruxandra@gmail.com>
2025-05-28 18:37:36 +0300

    added url endpoints

--- main.py
+++ main.py
@@ -21,6 +21,22 @@ def about(station, date):
             "date": date,
              "temperature": temperature}
 
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records") #dictionary with rows as records from df
+    return result
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20) #daca las parse_dates=["    DATE"], da eroare din cauza ca nu e string
+    df["    DATE"] = df["    DATE"].astype(str) #converts the date column to string
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records") #filters the dataframe by year and converts it to dictionary
+    return result
+
+
 #run the app
 if(__name__) == "__main__":
   app.run(debug=True) # port=5001 to run another time with this app. include it in app.run(..., port=5001)
